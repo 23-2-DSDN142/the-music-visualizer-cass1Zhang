1388d445414bb9a74212c471dd693f1f29c7afcb
cass1Zhang <140350029+cass1Zhang@users.noreply.github.com>
2023-10-16 13:13:30 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,78 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  colorMode(HSB, 100);
+  background(255)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+  strokeWeight (9)
+   
+   var ballSize = map(bass, 0, 100, 40, 150);
+   var drumHight = map(drum, 0, 100, height, 0+ballSize/2);
+   
+   
+   
+   fill(60,30,90);
+   ellipse(width/2,drumHight, ballSize);
+   
+   fill(60,200,90);
+   ellipse(width/4,drumHight, ballSize);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+   
+     var drumMap =map(drum, 0, 100, 5, 70);
+     var lengthOfLine = 300;
+     var LineStart = 100;
+     var lineEnd = LineStart+lengthOfLine;
+     stroke(drumMap,80,80);
+  
+     for(var i =1; i <= drumMap; i++){
+      var lineStep = i+20;
+      line(LineStart, lineStep, lineEnd, lineStep);
+     }
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-}
+     var drumMap =map(drum, 0, 50, 2, 40);
+     var lengthOfLine = 300;
+     var LineStart = 50;
+     var lineEnd = LineStart+lengthOfLine;
+     stroke(drumMap,40,40);
+  
+     for(var i =1; i <= drumMap; i++){
+      var lineStep = i+70;
+      line(LineStart, lineStep, lineEnd, lineStep);
+     }
+
+     var drumMap =map(drum, 0, 50, 2, 40);
+     var lengthOfLine = 220;
+     var LineStart = 20;
+     var lineEnd = LineStart+lengthOfLine;
+     stroke(drumMap,20,20);
+  
+     for(var i =1; i <= drumMap; i++){
+      var lineStep = i+123;
+      line(LineStart, lineStep, lineEnd, lineStep);
+     }
+
+     var drumMap =map(drum, 0, 50, 2, 40);
+     var lengthOfLine = 200;
+     var LineStart = 5;
+     var lineEnd = LineStart+lengthOfLine;
+     stroke(drumMap,20,60);
+  
+     for(var i =1; i <= drumMap; i++){
+      var lineStep = i+177;
+      line(LineStart, lineStep, lineEnd, lineStep);
+     }
+
+     var drumMap =map(drum, 0, 50, 2, 40);
+     var lengthOfLine = 100;
+     var LineStart = 5;
+     var lineEnd = LineStart+lengthOfLine;
+     stroke(drumMap,10,60);
+  
+     for(var i =1; i <= drumMap; i++){
+      var lineStep = i+230;
+      line(LineStart, lineStep, lineEnd, lineStep);
+     }
+    }
+  
